85c57e8cac5724d98fb0f5996c0a114657e24234
Pushpank Singh <pushpank1108@gmail.com>
2026-08-21 02:01:38 +0530

    backend deploy link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mansik-santulan-score.onrender.com";
+  const API_BASE = "https://mental-health-score-y8rh.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
